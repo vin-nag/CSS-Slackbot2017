81632eaddec55777fefa7d19d4c6a3aa1e2128a7
vin-nag <32803965+vin-nag@users.noreply.github.com>
2017-10-14 22:44:05 -0230

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const Botkit = require('botkit')
 const controller = Botkit.slackbot();
 
 const bot = controller.spawn({
-  token: config.TOKEN
+  token: TOKEN
 })
 
 bot.startRTM((err, bot, payload) => {
